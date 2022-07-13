f57cb7af4a8ce5e5e0726df3c6209fd55ceae621
Pooja Solaikannu <72905527+pooja-solaikannu@users.noreply.github.com>
2022-07-13 19:55:51 +0530

    updated removals and k in config

--- config/config.py
+++ config/config.py
@@ -2,3 +2,9 @@ TRAIN_DATA = "../data/flowers_desc_dataset.xlsx"
 DOC_TOPIC_DIST = "../data/doc_topic_dist.csv"
 COUNTVECT_MODEL = "../data/models/lda.pkl"
 LDA_MODEL = "../data/models/vectorizer.pkl"
+
+
+removals = ["url", "Soil pH Preferences", "Inflorescence Height", "Plant Spread", "Awards and Recognitions", "Parentage",
+            "Child plants", "Plant Height", "Minimum cold hardiness", "Toxicity", "Bloom Size"]
+
+k=5
