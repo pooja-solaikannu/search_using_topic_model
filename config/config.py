b9086d9d6d17356c537eca1c556cf4b980eabc8e
Pooja Solaikannu <72905527+pooja-solaikannu@users.noreply.github.com>
2022-07-13 19:57:39 +0530

    retified vectorier and lda model path

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 TRAIN_DATA = "../data/flowers_desc_dataset.xlsx"
 DOC_TOPIC_DIST = "../data/doc_topic_dist.csv"
-COUNTVECT_MODEL = "../data/models/lda.pkl"
-LDA_MODEL = "../data/models/vectorizer.pkl"
+LDA_MODEL = "../data/models/lda.pkl"
+VECTORIZER_MODEL = "../data/models/vectorizer.pkl"
 
 
 removals = ["url", "Soil pH Preferences", "Inflorescence Height", "Plant Spread", "Awards and Recognitions", "Parentage",
