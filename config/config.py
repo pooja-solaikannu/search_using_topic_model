6e9c43ba3886172860298509882e608d4eb144b9
Pooja Solaikannu <72905527+pooja-solaikannu@users.noreply.github.com>
2022-07-13 19:46:37 +0530

    update save location

--- config/config.py
+++ config/config.py
@@ -1,3 +1,4 @@
-TRAIN_DATA_PATH = ""
-
-MODEL_PATH = ""
+TRAIN_DATA = "../data/flowers_desc_dataset.xlsx"
+DOC_TOPIC_DIST = "../data/doc_topic_dist.csv"
+COUNTVECT_MODEL = "../data/models/lda.pkl"
+LDA_MODEL = "../data/models/vectorizer.pkl"
