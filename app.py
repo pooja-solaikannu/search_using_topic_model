e08de068d10eae0136714d6669bd08c87a0aad6a
Pooja Solaikannu <72905527+pooja-solaikannu@users.noreply.github.com>
2022-07-13 19:55:18 +0530

    mapped  static variables with config in app.py

--- app.py
+++ app.py
@@ -3,16 +3,8 @@ import pandas as pd
 
 from src.inference import *
 from src.data import *
+from config.config import *
 
-removals = ["url", "Soil pH Preferences", "Inflorescence Height", "Plant Spread", "Awards and Recognitions", "Parentage",
-            "Child plants", "Plant Height", "Minimum cold hardiness", "Toxicity", "Bloom Size"]
-
-filepath = "data/flowers_desc_dataset.xlsx"
-topic_dist_path = "data/doc_topic_dist.csv"
-lda_model_path = "data/models/lda.pkl"
-cv_model_path = "data/models/count_vec.pkl"
-
-k=5
 
 def op_transformation(sorted_op):
     related_flowers = {}
@@ -26,19 +18,19 @@ st.title("Topic Model based Searching System")
 st.info("🔍 Explore the available searching system, Thank you.")
 
 st.header("Data")
-df = data_cleaning(filepath=filepath)
+df = data_cleaning(filepath=TRAIN_DATA)
 st.text(f"Rows : {df.shape[0]}, Columns : {df.shape[1]}")
 st.write(df.head(5))
 
 
-count_vec, model, topic_dist = load_artifacts(cv_model_path, lda_model_path, topic_dist_path)
+vectorizer, model, doc_topic_dist = load_artifacts(VECTORIZER_MODEL, LDA_MODEL, DOC_TOPIC_DIST)
 st.header("Searching")
 query = st.text_input(label="", placeholder="Edible flowers for bees")
 if query:
     # get_model_results -> create src folder with model training and prediction script
     # transformed to the output sepcific format
-    doc_dist = pd.Series(model.transform(count_vec.transform([query]))[0])
-    k_nearest, k_distances = prediction(doc_dist, topic_dist, k)
+    doc_dist = pd.Series(model.transform(vectorizer.transform([query]))[0])
+    k_nearest, k_distances = prediction(doc_dist, doc_topic_dist, k)
     op = df[df["index"].isin(k_nearest)]
     op["k_dist"] = 1 - k_distances
     sorted_op = op.sort_values(by="k_dist", axis=0, ascending=False)
